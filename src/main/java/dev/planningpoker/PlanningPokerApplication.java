698cc7eecbda039a43dd35d3d45e7840895ddb9d
Dmytro Morsch <149238209+Dmytro-Morsch@users.noreply.github.com>
2023-06-28 20:49:55 +0300

    Initial commit

---
+++ src/main/java/dev/planningpoker/PlanningPokerApplication.java
@@ -0,0 +1,13 @@
+package dev.planningpoker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlanningPokerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PlanningPokerApplication.class, args);
+    }
+
+}
