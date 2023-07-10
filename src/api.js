6e8ba485fcb0dce2e727fb73a8abfb27db1bcb17
Dmytro Morsch <149238209+Dmytro-Morsch@users.noreply.github.com>
2023-07-10 06:29:16 +0200

    Check if response is not OK

--- src/api.js
+++ src/api.js
@@ -1,15 +1,23 @@
+function chekResponse(response) {
+    if (!response.ok) {
+        throw new Error(response.status)
+    }
+}
+
 const api = {
     createRoom: async (playerName) => {
         const response = await fetch('/api/room', {
             method: 'POST',
             body: playerName,
         });
+        chekResponse(response);
         return await response.json();
     },
     getVotes: async (roomId) => {
         const response = await fetch('/api/room/' + roomId, {
             method: 'GET',
         });
+        chekResponse(response);
         return await response.json();
     },
     addPlayerToRoom: async (roomId, playerName) => {
@@ -17,12 +25,14 @@ const api = {
             method: 'POST',
             body: playerName,
         });
+        chekResponse(response);
         return await response.json();
     },
     deletePlayer: async  (playerId) => {
         const response = await fetch('/api/player/' + playerId + '/delete', {
             method: 'POST'
         });
+        chekResponse(response);
         return await response.json();
     },
     vote: async (playerId, value) => {
@@ -30,18 +40,21 @@ const api = {
             method: 'POST',
             body: value
         });
+        chekResponse(response);
         return await response.json();
     },
     clearVotes: async (roomId) => {
         const response = await fetch('/api/room/' + roomId + '/clear', {
             method: 'POST'
         });
+        chekResponse(response);
         return await response.json();
     },
     showVotes: async (roomId) => {
-        await fetch('/api/room/' + roomId + '/show', {
+        const response = await fetch('/api/room/' + roomId + '/show', {
             method: 'POST'
         });
+        chekResponse(response);
     }
 };
 
