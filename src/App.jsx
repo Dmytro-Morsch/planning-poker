02ba2cb4c15bf04b3b5e60fdfe528abcfafcba33
Dmytro Morsch <149238209+Dmytro-Morsch@users.noreply.github.com>
2023-07-11 14:22:24 +0300

    Add reset.css

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
 import {Route, Routes} from "react-router-dom";
 import {PlayerProvider} from "./context/Game.context.jsx";
 
-import {Home, MainLayout, PageNotFound, Game} from './components';
+import {Game, Home, MainLayout, PageNotFound} from './components';
 
 import './App.css';
+import './reset.css';
 
 function App() {
     return (
