a86fdff35145a4f128b3b37dee84b9ad7af24072
Dmytro Morsch <149238209+Dmytro-Morsch@users.noreply.github.com>
2023-07-15 15:47:24 +0300

    Add styles to join game page

--- src/components/JoinGame/JoinGame.jsx
+++ src/components/JoinGame/JoinGame.jsx
@@ -1,5 +1,7 @@
 import {useState} from "react";
 
+import './JoinGame.css';
+
 function JoinGame({votes, onJoin}) {
     const [playerName, setPlayerName] = useState("");
     const [isNameTaken, setNameTaken] = useState(false);
@@ -18,18 +20,23 @@ function JoinGame({votes, onJoin}) {
     }
 
     return (
-        <>
+        <div className="join-game">
+            <h1 className="h1">Enter Game</h1>
             <form onSubmit={handleSubmit}>
-                <label>Display name:</label>
-                <input type="text" value={playerName} onChange={handleChange}/>
-                <button type="submit" disabled={isNameTaken}>Join</button>
+                <label className="custom-field">
+                    <input type="text" value={playerName} onChange={handleChange}/>
+                    <span className="placeholder">Display name *</span>
+                </label>
+                <button className="button join" type="submit" disabled={isNameTaken || !playerName}>Join</button>
                 {isNameTaken && <span>The name is taken</span>}
             </form>
-            <h3>Players in this game</h3>
-            {votes.map((vote) => (
-                <div key={`player-${vote.playerId}`}>{vote.playerName}</div>
-            ))}
-        </>
+            <h3 className="h3">Players in this game</h3>
+            <ul className="players">
+                {votes.map((vote) => (
+                    <li className="player" key={`player-${vote.playerId}`}>{vote.playerName}</li>
+                ))}
+            </ul>
+        </div>
     );
 }
 
