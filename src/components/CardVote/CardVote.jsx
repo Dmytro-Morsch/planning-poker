9d7c60cea8a25b410dfa21520134bd94d20729d2
Dmytro Morsch <149238209+Dmytro-Morsch@users.noreply.github.com>
2023-07-15 16:53:28 +0300

    Change star to color when card selected

--- src/components/CardVote/CardVote.jsx
+++ src/components/CardVote/CardVote.jsx
@@ -2,8 +2,8 @@ import './CardVote.css';
 
 function CardVote({value, isSelected, onSelect}) {
     return (
-        <div className="card" onClick={() => onSelect(value)}>
-            <span className={isSelected ? 'selected' : ''}>{value}</span>
+        <div className={"card" + (isSelected ? ' selected' : '')} onClick={() => onSelect(value)}>
+            <span>{value}</span>
         </div>
     );
 }
