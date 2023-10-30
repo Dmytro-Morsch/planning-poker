3011e9ea57b66bb3aaa4431a4261a75984c50954
Dmytro Morsch <149238209+dmytro-morsch@users.noreply.github.com>
2023-10-30 19:06:39 +0100

    Add citation from Wikipedia to home page

--- frontend/src/components/Home/Home.jsx
+++ frontend/src/components/Home/Home.jsx
@@ -78,6 +78,22 @@ function Home() {
                         </div>
                     </form>
                 </div>
+
+                <div className="row p-lg-5">
+                    <figure>
+                        <blockquote>
+                            Planning poker, also called Scrum poker, is a consensus-based, gamified technique for
+                            estimating, mostly used for timeboxing in Agile principles. In planning poker, members of
+                            the group make estimates by playing numbered cards face-down to the table, instead of
+                            speaking them aloud. The cards are revealed, and the estimates are then discussed. By hiding
+                            the figures in this way, the group can avoid the cognitive bias of anchoring, where the
+                            first number spoken aloud sets a precedent for subsequent estimates.
+                        </blockquote>
+                        <figcaption>
+                            <cite><a href="https://en.wikipedia.org/wiki/Planning_poker">Wikipedia</a></cite>
+                        </figcaption>
+                    </figure>
+                </div>
             </div>
         </div>
     );
